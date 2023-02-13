2f767d8966cf1159600fd3be72718e734f909413
Nikola Forró <nforro@redhat.com>
2023-02-13 20:46:01 +0100

    Test installation and import also on local system
    Signed-off-by: Nikola Forró <nforro@redhat.com>

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# fail early
+set -e
+
+
+### test import in virtualenvs
+
+pushd /rpm-shim
+tox
+popd
+
+
+### test import on local system
+
+PYTHON=python3
+PLATFORM_PYTHON=/usr/libexec/platform-python
+if [ ! -x "${PYTHON}" -a -x "${PLATFORM_PYTHON}" ]; then
+    # fallback to platform-python
+    PYTHON="${PLATFORM_PYTHON}"
+fi
+
+${PYTHON} -m build --wheel /rpm-shim
+# failure to install is most likely caused by existing RPM bindings, consider it a success
+${PYTHON} -m pip install /rpm-shim/dist/*.whl || exit 0
+${PYTHON} -c "import rpm"
+${PYTHON} -m pip check
